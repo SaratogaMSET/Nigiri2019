ef978758784d0884a2d7d4975cbb4a14996d1496
co-li <12012991+co-li@users.noreply.github.com>
2019-02-15 16:13:51 -0800

    Update Hatch Subsystem

--- src/main/java/frc/robot/subsystems/HatchSubsystem.java
+++ src/main/java/frc/robot/subsystems/HatchSubsystem.java
@@ -8,7 +8,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 
@@ -25,36 +25,36 @@ public class HatchSubsystem extends Subsystem {
     hatchDeploy
   }
 
-  public DoubleSolenoid hatchSol;
-  public DoubleSolenoid hatchDeploySol1;
-  public DoubleSolenoid hatchDeploySol2;
+  public Solenoid hatchSol;
+  public Solenoid hatchDeploySol1;
+  public Solenoid hatchDeploySol2;
   public DigitalInput hatchAcquired;
   public HatchPositions currentPosition;
 
   public HatchSubsystem() {
-    hatchSol = new DoubleSolenoid(RobotMap.Hatch.HATCH_PISTON[0], RobotMap.Hatch.HATCH_PISTON[1]);
-    hatchDeploySol1 = new DoubleSolenoid(RobotMap.Hatch.HATCH_DEPLOY_PISTON_1[0], RobotMap.Hatch.HATCH_DEPLOY_PISTON_1[1]);
-    hatchDeploySol2 = new DoubleSolenoid(RobotMap.Hatch.HATCH_DEPLOY_PISTON_1[0], RobotMap.Hatch.HATCH_DEPLOY_PISTON_2[1]);
+    hatchSol = new Solenoid(RobotMap.Hatch.HATCH_PISTON[0], RobotMap.Hatch.HATCH_PISTON[1]);
+    hatchDeploySol1 = new Solenoid(RobotMap.Hatch.HATCH_DEPLOY_PISTON_1[0], RobotMap.Hatch.HATCH_DEPLOY_PISTON_1[1]);
+    hatchDeploySol2 = new Solenoid(RobotMap.Hatch.HATCH_DEPLOY_PISTON_1[0], RobotMap.Hatch.HATCH_DEPLOY_PISTON_2[1]);
     hatchAcquired = new DigitalInput(RobotMap.Hatch.HATCH_SWITCH);
     currentPosition = HatchPositions.hatchIn;
   }
 
   public void hatchOut() {
-    hatchSol.set(DoubleSolenoid.Value.kForward);
+    hatchSol.set(true);
   }
 
   public void hatchIn() {
-    hatchSol.set(DoubleSolenoid.Value.kReverse);
+    hatchSol.set(false);
   }
 
   public void hatchDeploy() {
-    hatchDeploySol1.set(DoubleSolenoid.Value.kForward);
-    hatchDeploySol2.set(DoubleSolenoid.Value.kForward);
+    hatchDeploySol1.set(true);
+    hatchDeploySol2.set(true);
   }
 
   public void hatchDeployIn() {
-    hatchDeploySol1.set(DoubleSolenoid.Value.kReverse);
-    hatchDeploySol2.set(DoubleSolenoid.Value.kReverse);
+    hatchDeploySol1.set(false);
+    hatchDeploySol2.set(false);
   }
 
   public boolean getHatchAcquired() {
