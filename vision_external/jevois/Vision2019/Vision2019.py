b1e425b40f9ad97e214cc090ae2f9e070c2d1a5b
Dhruv Shah <dhruv.shah@gmail.com>
2019-03-03 22:17:16 -0800

    vision noise filtering & PnP error deformation

--- vision_external/jevois/Vision2019/Vision2019.py
+++ vision_external/jevois/Vision2019/Vision2019.py
@@ -19,11 +19,13 @@ import math
 # @restrictions None
 # @ingroup modules
 
-DEBUG = 0
+DEBUG = 1
 FOCAL_LENGTH = 1732.050808
 ANGLE_MULTIPLIER = math.cos(math.radians(14.5))
 TARGET_WIDTH_FT = (2.0/12.0)*ANGLE_MULTIPLIER
-FOV = 15.0
+FOV = 18.0
+
+CAMERA_OFFSET_IN = 6.3125
 
 class Vision2019:
     # ###################################################################################################
@@ -35,18 +37,20 @@ class Vision2019:
         #      105=light blue, 120=blue, 135=purple, 150=pink
         # S: 0 for unsaturated (whitish discolored object) to 255 for fully saturated (solid color)
         # L: 0 for dark to 255 for maximally bright
-        self.HLSmin = np.array([ 30, 0, 230], dtype=np.uint8)
-        self.HLSmax = np.array([ 70, 255, 255], dtype=np.uint8)
+        self.HLSmin = np.array([ 65, 100, 0], dtype=np.uint8)
+        self.HLSmax = np.array([ 105, 255, 255], dtype=np.uint8)
         # Other processing parameters:
         self.epsilon = 0.02              # Shape smoothing factor (higher for smoother)
-        self.hullarea = ( 10*10, 300*200 ) # Range of object area (in pixels) to track
-        self.hullfill = 97                # Min fill ratio of the convex hull (percent)
+        self.hullarea = ( 5*5, 300*200 ) # Range of object area (in pixels) to track
+        self.hullfill = 87                # Min fill ratio of the convex hull (percent)
         self.ethresh = 5                 # Shape error threshold (lower is stricter for exact shape)
         self.margin = 0                    # Margin from from frame borders (pixels)
         self.bound_ratio_error = 60        # % error on bounding aspect ratio
         self.bound_target_ratio = (0.52419)    # target on bounding aspect ratio
         self.rot_ratio_error = 20           # % error on rotated bounding aspect ratio
         self.rot_target_ratio = (0.444, 2.333) # the two rotated rectangle aspect ratios (one horizontal, one for vertical)
+
+        self.prev_center = [0.0, 0.0]
 
         # Instantiate a JeVois Timer to measure our processing framerate:
         self.timer = jevois.Timer("Vision2019", 100, jevois.LOG_INFO)
@@ -72,7 +76,7 @@ class Vision2019:
     # ###################################################################################################
     ## Detect objects within our HSV range
     def detect(self, imgbgr, outimg = None):
-        maxn = 5 # max number of objects we will consider
+        maxn = 20 # max number of objects we will consider
         h, w, chans = imgbgr.shape
         # Convert input image to HSV:
         imghls = cv2.cvtColor(imgbgr, cv2.COLOR_BGR2HLS)
@@ -82,13 +86,14 @@ class Vision2019:
             self.erodeElement = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
             self.dilateElement = cv2.getStructuringElement(cv2.MORPH_RECT, (4, 4))
 
+        # Isolate pixels inside our desired HLS range:
+        imgth = cv2.inRange(imghls, self.HLSmin, self.HLSmax)
+
         # Apply morphological operations to cleanup the image noise:
         imghls = cv2.erode(imghls, self.erodeElement)
         imghls = cv2.dilate(imghls, self.dilateElement)
 
-        # Isolate pixels inside our desired HLS range:
-        imgth = cv2.inRange(imghls, self.HLSmin, self.HLSmax)
-        imgth = cv2.blur(imgth, (5, 5))
+        #imgth = cv2.blur(imgth, (3, 3))
 
         # Detect objects by finding contours:
         contours, hierarchy = cv2.findContours(imgth, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
@@ -99,6 +104,11 @@ class Vision2019:
         contour_matches = []
         # Array of cost, center
         contour_costs = []
+        
+        # display threshold
+        #resultBGR = cv2.bitwise_and(imgbgr, imgbgr, mask = imgth)
+        #display = cv2.cvtColor(resultBGR, cv2.COLOR_BGR2RGB)
+        jevois.convertCvGRAYtoRawImage(imgth, outimg, 100)
 
         # Identify the "good" objects:
         for c in contours:
@@ -157,20 +167,8 @@ class Vision2019:
             else:
                 match += "E" # Shape error is ok
 
-            # Get bounding reectangle (no rotation) for aspect ratio
-            _, _, bound_rect_w, bound_rect_h = cv2.boundingRect(c)
-            bound_aspect_ratio = float(bound_rect_w)/float(bound_rect_h)
-            bound_ratio_err = round(abs(bound_aspect_ratio - self.bound_target_ratio)/bound_aspect_ratio * 100)
-            self.logMessage(bound_ratio_err, "bounding aspect ratio error ")
-            if bound_ratio_err > self.bound_ratio_error:
-                match += ""
-            else:
-                match += "B" # Bounding ratio error is ok
-
-            cost += math.sqrt(bound_ratio_err)
-
             # Get bounding box for rotated rectangle
-            rot_box = cv2.minAreaRect(hull) # ((x,y), (w, h), theta)
+            rot_box = cv2.minAreaRect(c) # ((x,y), (w, h), theta)
             rot_w,rot_h = rot_box[1]
             rot_w = float(rot_w)
             rot_h = float(rot_h)
@@ -180,12 +178,12 @@ class Vision2019:
             box = np.int0(box)
             rectArea = cv2.contourArea(box)
 
-            rect_fill = area/rectArea * 100.0
-            if rect_fill < self.hullfill:
-                continue
+            for a in box:
+                for b in box:
+                    jevois.drawLine(outimg, int(a[0]), int(a[1]), int(b[0]), int(b[1]), 1, jevois.YUYV.MedPink)
 
             self.logMessage((rot_w/rot_h, theta), "ROT RECT ")
-            jevois.LINFO("theta {}".format(theta))
+            #jevois.LINFO("theta {}".format(theta))
 
             rot_aspect_ratio = float(rot_w)/float(rot_h)
             rot_ratio_err = round(min(abs(rot_aspect_ratio - self.rot_target_ratio[0]), abs(rot_aspect_ratio - self.rot_target_ratio[1])))/rot_aspect_ratio * 100
@@ -207,16 +205,13 @@ class Vision2019:
 
             cost += (10 - len(match))
 
-            contour_matches.append((match, center, min(rot_w, rot_h)*ANGLE_MULTIPLIER, theta))
-            contour_costs.append((cost, center, min(rot_w, rot_h)*ANGLE_MULTIPLIER, theta))
+            contour_matches.append((match, center, min(rot_w, rot_h)*ANGLE_MULTIPLIER, theta, box))
+            contour_costs.append((cost, center, min(rot_w, rot_h)*ANGLE_MULTIPLIER, theta, box))
 
-        # display threshold
-        #resultBGR = cv2.bitwise_and(imgbgr, imgbgr, mask = imgth)
-        #display = cv2.cvtColor(resultBGR, cv2.COLOR_BGR2RGB)
-        jevois.convertCvGRAYtoRawImage(imgth, outimg, 100)
+        
 
         if len(contour_matches) == 0:
-            #jevois.LERROR("NO CONTOUR DETECTED")
+            jevois.LERROR("NO CONTOUR DETECTED")
             return None, None, None
 
         if len(contour_costs) == 1:
@@ -232,7 +227,8 @@ class Vision2019:
         found = []
         for i in range(0, len(contour_costs)):
             for j in range (i+1, len(contour_costs)):
-                if abs(contour_costs[i][3]) > 70 and abs(contour_costs[j][3]) < 70:
+                #self.logMessage(contour_costs[i])
+                if abs(abs(contour_costs[i][3]) - abs(contour_costs[j][3])) > 40:
                     found.append((contour_costs[i], contour_costs[j]))
 
         found.sort(key=self.doubleContourSort)
@@ -240,14 +236,39 @@ class Vision2019:
             found = found[0]
         except:
             return None, None, None
-        centers = np.array([center for (_, center, _, _) in found]) # two lowest cost contours
-        target_widths = np.array([self.calculateDistance(w, target_w) for (_, _, target_w, _) in found]) # two lowest cost contours
+
+                #jevois.LINFO("{}{}".format(found[0][3], found[1][3]))
+        centers = np.array([center for (_, center, _, _, _) in found]) # two lowest cost contours
 
         center = tuple(np.mean(centers, axis=0, dtype=np.float64).astype(np.int))
         center = [a.item() for a in center]
 
-        avg_d = np.mean(target_widths, axis=0, dtype=np.float64)
-        return centers, center, avg_d
+        self.prev_center = center
+
+        model_points = np.array([
+                                 (0.0, 0.0, 0.0),             # sorted by X
+                                 (1.38, 5.32, 0.0),             # Nose tip
+                                 (1.94, -0.5, 0.0),             # Nose tip
+                                 (3.31, 4.82, 0.0),             # Nose tip
+                                 (11.31, 4.82, 0.0),             # Nose tip
+                                 (12.69, -0.5, 0.0),             # Nose tip
+                                 (13.25, 5.32, 0.0),             # Nose tip
+                                 (14.63, 0.0, 0.0),             # Nose tip
+                                 ], dtype=np.float64)
+
+        self.logMessage("f {}".format(found[0][4]))
+
+        c1 = found[0][4]
+        c2 = found[1][4]
+        fullContour = np.squeeze(np.concatenate((c1, c2), axis=0)).astype(np.float64)
+        fullContour = fullContour[fullContour[:,0].argsort()]
+        self.logMessage("fullContour {}".format(fullContour))
+
+
+        success, rotation_vector, translation_vector = cv2.solvePnP(model_points, fullContour, self.camMatrix, self.distCoeffs)
+        self.logMessage("TV {}".format(translation_vector[2]))
+
+        return centers, center, math.sqrt(translation_vector[2][0] * translation_vector[2][0] + translation_vector[1][0] * translation_vector[1][0] + translation_vector[0][0] * translation_vector[0][0]), success
     # ###################################################################################################
     ## Caclulate angle displacement
     def calculateAngle(self, w, h, center):
@@ -263,6 +284,15 @@ class Vision2019:
             return 0.0
         d = TARGET_WIDTH_FT * w / (2.0 * target_width * math.tan(math.radians(FOV)))
         return d
+
+    def calculateCorrectedAngle(self, raw_angle, distance, camera_offset_inches):
+        #d = math.sqrt(distance*distance + camera_offset_inches*camera_offset_inches - 2 * camera_offset_inches * distance * math.sin(math.radians(raw_angle)))
+        #correctedAngle = math.degrees(math.asin(math.cos(math.radians(raw_angle)) * distance/d))-90.0
+            #if abs(math.sin(math.radians(raw_angle)))*distance > camera_offset_inches:
+            #correctedAngle = -correctedAngle
+        theta1 = math.degrees(math.atan((-5.0 - distance * math.cos(math.radians(raw_angle)))/(6.3125 - distance * math.sin(math.radians(raw_angle)))))
+        correctedAngle = math.copysign(1, theta1) * (90 - math.copysign(1, theta1) * theta1)
+        return correctedAngle
 
     def logMessage(self, msg, prefix_msg = ""):
         if DEBUG != 1: return
@@ -319,16 +349,18 @@ class Vision2019:
         jevois.paste(inimg, outimg, 0, 0)
 
         # Get the centers of the vision targets and the center of the pair
-        centers, center, dist = self.detect(imgbgr, outimg)
+        centers, center, dist, success = self.detect(imgbgr, outimg)
         angle = None
         if center != None:
             # Send serial messages
             angle = self.calculateAngle(w, h, center)
             # Draw detections on image
             self.drawDetections(outimg, centers, center)
-        if angle != None and dist != None:
-            jevois.sendSerial("ANGLE {}".format(angle))
+        if angle != None and dist != None and success and dist > 48.0 and dist < 3600.0:
+            #jevois.sendSerial("RAW_ANGLE {}".format(angle))
             jevois.sendSerial("DISTANCE {}".format(dist))
+            jevois.sendSerial("ANGLE {}".format(self.calculateCorrectedAngle(angle, dist, CAMERA_OFFSET_IN)))
+
         # Write frames/s info from our timer into the edge map (NOTE: does not account for output conversion time):
         fps = self.timer.stop()
 
@@ -350,5 +382,10 @@ class Vision2019:
     def contourX(self, item):
         return item[1][0]
 
+    def euclidDist(self, a, b):
+        return math.sqrt((b[0] - a[0])*(b[0] - a[0]) + (b[1] - a[1])*(b[1] - a[1]))
+
     def doubleContourSort(self, item):
-        return item[0][0] * item[1][0]
+        doubleContourCenter = tuple(np.mean([item[0][1], item[1][1]], axis=0, dtype=np.float64).astype(np.int))
+        self.logMessage(doubleContourCenter)
+        return item[0][0] + item[1][0] + self.euclidDist(item[0][1], item[1][1]) + 50 * self.euclidDist(self.prev_center, doubleContourCenter)
