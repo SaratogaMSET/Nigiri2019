696593b9a9965693a7265bee4d1fe78d774bacef
co-li <12012991+co-li@users.noreply.github.com>
2019-03-10 17:16:09 -0700

    Add auto paths

--- src/main/java/com/team319x649/FishyPathCreator.java
+++ src/main/java/com/team319x649/FishyPathCreator.java
@@ -31,11 +31,10 @@ public class FishyPathCreator extends AbstractFishyPathCreator {
 	
 	private FishyPathCreator() {
 		config.dt = 0.02;
-		config.max_acc = 8.0; // Maximum acceleration in FPS^2
-		config.max_vel = 12.0; // Maximum velocity in FPS
+		config.max_acc = 8.0; // Maximum acceleration in FPS^
+		config.max_vel = 12.0; // Maximum velocity in FP
 		config.max_jerk = 2.0;
 	}
-
     @Override
     protected List<FishyPath> getPaths() {
 		List<FishyPath> paths = new ArrayList<>();
@@ -50,10 +49,10 @@ public class FishyPathCreator extends AbstractFishyPathCreator {
 	 * @return the list of team paths to generate
 	 */
 	private List<FishyPath> generateTeamPaths() {
-		 // Create a path with the name of "Example", this will generate a file named ExampleArc
+		 // Create a path with the name of"Example", this will generate a file named ExampleArc
 		 FishyPath exampleArc = new FishyPath(config, "FarRocketLeft", DrivetrainSubsystem.WHEELBASE_FEET);
 		 // Set the first point to the starating point, this be done with any of the addWaypoint methods
-		 // positive X is forward, positive Y is left, units are in feet and degrees
+		 // positive X is forward, positive Y is left, units ae in feet and degrees
 		 exampleArc.addWaypoint(startingPoint);
 		 // Add the next point that 4.5 ft forward, and doesn't turn, it also has a max speed of 4 FPS, 
 		 // it will arrive at this location going 4 FPS
@@ -61,8 +60,44 @@ public class FishyPathCreator extends AbstractFishyPathCreator {
 		 // Add the next point to be at global coordinates
 		 exampleArc.addWaypoint(25, 24, 0, 0, 11.5);
 		//  exampleArc.addWaypointRelative(-3.3, 0.675, -29.5, 0, 10.0);
-		 
-		 return asList(exampleArc); // return asList(path1, path2, path3, ...);
+		
+		FishyPath frontCargo = new FishyPath(config, "HABM-CF", DrivetrainSubsystem.WHEELBASE_FEET);
+		frontCargo.addWaypoint(5.5, 14.5, 0, 0, 0);
+		frontCargo.addWaypointRelative(10.5, 0, 0, 0, 11.5);
+
+		FishyPath nearCargoShip = new FishyPath(config, "HAB1L-CL1", DrivetrainSubsystem.WHEELBASE_FEET);
+		nearCargoShip.addWaypoint(startingPoint);
+		nearCargoShip.addWaypointRelative(4, 0, 0, 3, 3);
+		nearCargoShip.addWaypointRelative(6, 3, 25, 10, 11.5);
+		nearCargoShip.addWaypoint(21.7, 18, 89.99, 0, 11.5);
+
+		FishyPath frontToLoadingStation1 = new FishyPath(config, "CLF-LSL-1", DrivetrainSubsystem.WHEELBASE_FEET);
+		frontToLoadingStation1.addWaypoint(16, 14.5, 0, 0, 0);
+		frontToLoadingStation1.addWaypointRelative(-3, 3, 89.99, 1, 3);
+		frontToLoadingStation1.addWaypoint(16, 20.5, 179.98, 0, 3);
+
+		FishyPath frontToLoadingStation2 = new FishyPath(config, "CLF-LSL-2", DrivetrainSubsystem.WHEELBASE_FEET);
+		frontToLoadingStation2.addWaypoint(16, 20.5, 0, 0, 0);
+		frontToLoadingStation2.addWaypoint(1.5, 24.75, 0, 0, 11.5);
+		
+		FishyPath loadingToNearRocketLeft1 = new FishyPath(config, "LSL-RLL-1", DrivetrainSubsystem.WHEELBASE_FEET);
+		loadingToNearRocketLeft1.addWaypoint(1.5, 24.75, 0, 0, 0);
+		loadingToNearRocketLeft1.addWaypoint(10, 18, 89.99, 0, 11.5);
+
+		FishyPath loadingToNearRocketLeft2 = new FishyPath(config, "LSL-RLL-2", DrivetrainSubsystem.WHEELBASE_FEET);
+		loadingToNearRocketLeft2.addWaypoint(10, 18, 89.99, 0, 0);
+		loadingToNearRocketLeft2.addWaypoint(14.5, 23.75, 30, 0, 11.5);
+
+		FishyPath loadingToMidCargoShip1 = new FishyPath(config, "LSL-CL2-1", DrivetrainSubsystem.WHEELBASE_FEET);
+		loadingToMidCargoShip1.addWaypoint(1.5, 24.75, 0, 0, 0);
+		loadingToMidCargoShip1.addWaypointRelative(15, -5, 15, 10, 11.5);
+		loadingToMidCargoShip1.addWaypoint(25, 24, 30, 0, 11.5);
+
+		FishyPath loadingToMidCargoShip2 = new FishyPath(config, "LSL-CL2-2", DrivetrainSubsystem.WHEELBASE_FEET);
+		loadingToMidCargoShip2.addWaypoint(25, 24, 30, 0, 0);
+		loadingToMidCargoShip2.addWaypoint(23.5, 18, 89.99, 0, 11.5);
+
+		 return asList(exampleArc, frontCargo, nearCargoShip, frontToLoadingStation1, frontToLoadingStation2, loadingToNearRocketLeft1, loadingToNearRocketLeft2, loadingToMidCargoShip1, loadingToMidCargoShip2); // return asList(path1, path2, path3, ...);
 	}
 	
 	
